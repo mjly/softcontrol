b9ccd38ae17e1a9fab1ec4e808a6fc1404e03f2e
yuanluolove <yuanluolove@163.com>
2018-05-31 18:26:54 +0800

    修改用户信息代码

--- server/main.py
+++ server/main.py
@@ -1,7 +1,6 @@
 from handlers.soft_manager import *
 from handlers.main_handlers import *
-
-
+from handlers.user_manager import *
 def MainProcess():  # 主过程，程序的入口
     tornado.options.parse_command_line()
     application = tornado.web.Application([  # 这里就是路由表，确定了哪些URL由哪些Handler响应
@@ -10,6 +9,11 @@ def MainProcess():  # 主过程，程序的入口
         url(r'/soft/EditSoft', EditSoftHandler),
         url(r'/soft/DeleteSoft', DeleteSoftHandler),
         url(r'/soft/UpdateSoftInfo', UpdateSoftInfoHandler),
+        url(r'/user/', UserMainHandler),  # 路由表中的URL是用正则表达式来过滤的
+        url(r'/user/Adduser', AddUserHandler),
+        url(r'/user/EditUser', EditUserHandler),
+        url(r'/user/DeleteUser', DeleteUserHandler),
+        url(r'/user/UpdateUserInfo', UpdateUserInfoHandler),
         url(r"/install", MyFormHandler)
     ])
 
