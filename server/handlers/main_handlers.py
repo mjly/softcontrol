f08ef34a774d14c950f75313a115a13504d4c5b2
yuanluolove <yuanluolove@163.com>
2018-05-31 08:14:16 +0800

    修改服务器和客户端agent之间的通讯代码

---
+++ server/handlers/main_handlers.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy.testing import db
+from tornado.web import RequestHandler,Application,url
+import tornado.ioloop
+import tornado.web
+from handlers.send_msg import *
+class MyFormHandler(tornado.web.RequestHandler):
+    def get(self):
+            self.write('<html><body><form action="/install" method="POST">'
+               #  '<input type="text" name="message">'
+                 '<input type="submit" value="安装软件">'
+                 '</form></body></html>')
+
+
+
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        ipaddr=self.request.remote_ip
+        #print(ipaddr)
+        msg = sendMsg(ipaddr,'getinfo')
+
+        filename,md5,sha1,username = msg.split(' ', 4);
+        #print(filename)
+        #print(md5)
+        #print(sha1)
+        #print(username)
+        #print(msg)
+        self.write("You wrote " +msg)
+        #msg =sendMsg(ipaddr, 'install')
+        #print(msg)
